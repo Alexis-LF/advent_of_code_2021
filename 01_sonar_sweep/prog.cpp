11f1c90b41337d92b3bd328198f1a534e7fb950a
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2021-12-01 14:17:38 +0100

    part2

--- 01_sonar_sweep/prog.cpp
+++ 01_sonar_sweep/prog.cpp
@@ -43,12 +43,29 @@ int part1(vector<int> reports)
     return count;
 }
 
+int part2(vector<int> reports)
+{
+    unsigned int count = 0;
+    for (int i = 3; i < reports.size(); ++i)
+    {
+        int sumA = reports[i-3] + reports[i-2] + reports[i-1];
+        int sumB = reports[i-2] + reports[i-1] + reports[i];
+        if (sumB > sumA)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main()
 {
     vector<int> reports = get_int_inputs();
     int result_p1 = part1(reports);
+    int result_p2 = part2(reports);
 
     cout << "\nthere are " << result_p1 << " measurements that are larger than the previous measurement." << endl;
+    cout << "\nthere are " << result_p2 << " sums are larger than the previous sum." << endl;
 
     return 0;
 }
